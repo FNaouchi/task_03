9a538e53a908bd3e4fefa674197256ce945eb780
FNaouchi <f.naoushi@hotmail.com>
2018-10-07 15:48:07 +0300

    task submission

--- restaurants/views.py
+++ restaurants/views.py
@@ -5,15 +5,32 @@ def welcome(request):
 
 def restaurant_list(request):
 
-    context = {
 
-    }
+    context = {
+    	"my_list": [
+    					{
+    						"restaurant_name": "MacDonalds",
+    						"food_type": "Burger"
+    					},
+    					{
+    						"restaurant_name": "KFC",
+    						"food_type": "Fried Chicken"
+    					},
+    					{
+    						"restaurant_name": "Starbucks",
+    						"food_type": "Drinks"
+    					},
+    				]
+    		}
     return render(request, 'list.html', context)
 
 
 def restaurant_detail(request):
 
     context = {
-
+    	"my_object": {
+    					"restaurant_name": "Starbucks",
+    					"food_type": "Drinks"
+    				},
     }
     return render(request, 'detail.html', context)
